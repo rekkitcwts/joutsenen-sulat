226d10b4001b34e2336c7c993d61b4b1edb62040
Dynse Clyde Sacote <clydedatastruct@gmx.com>
2025-08-10 22:27:11 +0800

    Commented broken sections for now

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -16,10 +16,10 @@ export default function Home() {
       </header>
       
       <main>
-        <HeroSection />
+        /*<HeroSection />
         <ChannelShowcase />
         <EPGPreview />
-        <ServiceAreas />
+        <ServiceAreas />*/
       </main>
       
       <footer>
